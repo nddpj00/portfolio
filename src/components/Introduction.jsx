879fcf77f06584b99d527bfd9188c412c3c452e2
Dan Jones <108077282+nddpj00@users.noreply.github.com>
2024-05-07 21:38:48 +0100

    alters react typed to shorten and split between 3 different strings

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -4,12 +4,10 @@ import { ReactTyped } from "react-typed"; // Corrected import
 export default function Introduction() {
   const [showIntro, setShowIntro] = useState(false);
 
-  const introText = `
-  Welcome to my portfolio site! I'm Daniel Jones, a dynamic Software Developer with a rich background in sales, now fully immersed in the tech industry. With over two decades of experience honing my communication and project management skills, I've transitioned successfully into software development, earning a Level 5 Diploma in Web Application Development.
-  
-  My portfolio showcases a range of projects that highlight my expertise in HTML, CSS, JavaScript, and Python, as well as frameworks like React, Flask, and Django. From non-profit initiatives to bespoke e-commerce solutions for local businesses, I'm committed to crafting innovative, user-centric digital solutions.
-  
-  Explore my projects, filter them by technology, and don't hesitate to get in touch via email. Let's connect and discuss how I can contribute to your next project or venture!`;
+  const introText = `Welcome to my portfolio site! I'm <strong>Dan Jones</strong>, a creative Software Developer.`;
+
+  const middleText = `My portfolio showcases a range of projects I've worked on, from e-commerce sites to this site you are on right now!`;
+  const endText = `Explore my projects, filter them by technology, and don't hesitate to get in touch via email. Let's connect and discuss how I can contribute to your next project or venture!`;
 
   const displayIntro = () => {
     setShowIntro(!showIntro);
@@ -17,7 +15,7 @@ export default function Introduction() {
 
   return (
     <div className="mb-4">
-      <div className="text-xl justify-items-center">
+      <div className="text-xl justify-items-center font-league_spartan">
         <img
           src="https://res.cloudinary.com/dhljiows0/image/upload/v1714142944/media/my_projects/images/About_Me_109x50_cjsv9q.png"
           alt="about me keys"
@@ -27,10 +25,9 @@ export default function Introduction() {
       <div>
         {showIntro && (
           <ReactTyped
-            strings={[introText]}
-            typeSpeed={40}
-            backSpeed={50}
-            loop
+            strings={[introText, middleText, endText]}
+            typeSpeed={35}
+            backSpeed={20}
           />
         )}
       </div>
